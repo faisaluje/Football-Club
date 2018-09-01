cb49237474d02255c495bd0a6d61d5f2a24cddda
Faisal Uje <faisal.uje@gmail.com>
2018-09-01 13:34:46 +0700

    add/remove favorite club

--- app/src/main/java/com/faisaluje/footballclubwithapi/detail/TeamDetailActivity.kt
+++ app/src/main/java/com/faisaluje/footballclubwithapi/detail/TeamDetailActivity.kt
@@ -16,6 +16,8 @@ import android.widget.TextView
 import com.faisaluje.footballclubwithapi.R
 import com.faisaluje.footballclubwithapi.R.color.colorAccent
 import com.faisaluje.footballclubwithapi.R.color.primary_text_default_material_light
+import com.faisaluje.footballclubwithapi.R.drawable.ic_added_to_favorites
+import com.faisaluje.footballclubwithapi.R.drawable.ic_add_to_favorites
 import com.faisaluje.footballclubwithapi.api.ApiRepository
 import com.faisaluje.footballclubwithapi.db.Favorite
 import com.faisaluje.footballclubwithapi.db.database
@@ -25,7 +27,10 @@ import com.faisaluje.footballclubwithapi.util.visible
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
 import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
@@ -49,6 +54,11 @@ class TeamDetailActivity: AppCompatActivity(), TeamDetailView{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val intent = intent
+        id = intent.getStringExtra("id")
+        supportActionBar?.title = "Team Detail"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         linearLayout {
             lparams(width = matchParent, height = matchParent)
@@ -103,11 +113,8 @@ class TeamDetailActivity: AppCompatActivity(), TeamDetailView{
             }
         }
 
-        supportActionBar?.title = "Team Detail"
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        favoriteState()
 
-        val intent = intent
-        id = intent.getStringExtra("id")
         val request = ApiRepository()
         val gson = Gson()
         presenter = TeamDetailPresenter(this, request, gson)
@@ -121,6 +128,7 @@ class TeamDetailActivity: AppCompatActivity(), TeamDetailView{
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.detail_menu, menu)
         menuItem = menu
+        setFavorite()
 
         return true
     }
@@ -132,7 +140,11 @@ class TeamDetailActivity: AppCompatActivity(), TeamDetailView{
                 true
             }
             R.id.add_to_favorite -> {
-                addToFavorite()
+                if (isFavorite) removeFromFavorite() else addToFavorite()
+
+                isFavorite = !isFavorite
+                setFavorite()
+
                 true
             }
 
@@ -170,6 +182,31 @@ class TeamDetailActivity: AppCompatActivity(), TeamDetailView{
             snackbar(swipeRefresh, "Added to favorite").show()
         }catch (e: SQLiteConstraintException){
             snackbar(swipeRefresh, e.localizedMessage).show()
+        }
+    }
+
+    private fun removeFromFavorite(){
+        try {
+            database.use{
+                delete(Favorite.TABLE_FAVORITE, "(TEAM_ID = {id})", "id" to id)
+            }
+            snackbar(swipeRefresh, "Removed to favorite").show()
+        } catch (e: SQLiteConstraintException){
+            snackbar(swipeRefresh, e.localizedMessage).show()
+        }
+    }
+
+    private fun setFavorite(){
+        if (isFavorite) menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, ic_added_to_favorites)
+        else menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, ic_add_to_favorites)
+    }
+
+    private fun favoriteState(){
+        database.use {
+            val result = select(Favorite.TABLE_FAVORITE)
+                    .whereArgs("(TEAM_ID = {id})", "id" to id)
+            val favorite = result.parseList(classParser<Favorite>())
+            if (!favorite.isEmpty()) isFavorite = true
         }
     }
 }
